b7ebbf5a10697186428fa591102a99185d50a269
MAYLON LUIZ SANTOS OLIVEIRA DO NASCIMENTO <maylon.lsonascimento@senacsp.edu.br>
2025-01-14 15:20:52 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Sou Maylon");
+  Console.WriteLine ("Estou aprendendo Lógica de programaçao");
---
+++ ex03/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Meu nome é Maylon"); 
+Console.WriteLine("Estou estudando no Senac largo treze");
